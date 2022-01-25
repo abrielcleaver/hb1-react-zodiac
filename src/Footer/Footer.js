090923b012a3c27448290b8f20e9a5d957bbf882
Abriel Cleaver <abrielcleaver@gmail.com>
2022-01-24 18:48:43 -0700

    fixing eslint issues

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import './Footer.css'
+import React from 'react';
+import './Footer.css';
 
 export default function Footer(props) {
-  return <footer>Copyright Alchemy Codelab {props.year}</footer>
+  return <footer>Copyright Alchemy Codelab {props.year}</footer>;
 }
